d5513d8ce30bcc5e7f73bbcaeff308055c428c18
Guhan <guhan3697figmentmajesty@gmail.com>
2025-08-05 11:30:40 +0000

    Make Good Model Registration_2

--- sales/views.py
+++ sales/views.py
@@ -1,14 +1,3 @@
 from django.shortcuts import render
-from django.views.generic import ListView
-from .models import Ad
 
-class AdListView(ListView):
-    model = Ad
-    template_name = 'AdListView.html'
-    context_object_name = 'ads'
-    
-    paginate_by = 10
-    
-    def get_queryset(self):
-        # We only want to show active ads
-        return Ad.objects.filter(is_active=True).order_by('-created_at')
+# Create your views here.
