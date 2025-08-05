3a96139217e1359be4f49f237563422b28ffe826
Guhan <guhan3697figmentmajesty@gmail.com>
2025-08-05 07:11:36 +0000

    Add ListView for displaying Ad items
    Implemented a ListView to display a list of Ad objects

--- sales/admin.py
+++ sales/admin.py
@@ -48,3 +48,35 @@ class MessageAdmin(admin.ModelAdmin):
     search_fields = ['content']
     date_hierarchy = 'sent_at'
     raw_id_fields = ['sender', 'recipient', 'ad']
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ['username', 'email', 'first_name', 'last_name', 'is_staff', 'phone_number']
+    fieldsets = UserAdmin.fieldsets + (
+        ('Contact Information', {'fields': ('phone_number', 'contact_info_visibility')}),
+        ('Profile', {'fields': ('profile_picture',)}),
+    )
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description']
+    search_fields = ['name']
+
+
+@admin.register(Ad)
+class AdAdmin(admin.ModelAdmin):
+    list_display = ['title', 'user', 'category', 'ad_type', 'price', 'is_active', 'created_at']
+    list_filter = ['ad_type', 'category', 'is_active', 'created_at']
+    search_fields = ['title', 'description', 'location']
+    date_hierarchy = 'created_at' # Provides a drill-down navigation by date
+    raw_id_fields = ['user', 'category'] # Use a searchable input for foreign keys
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ['ad', 'sender', 'recipient', 'sent_at', 'read']
+    list_filter = ['read', 'sent_at']
+    search_fields = ['content']
+    date_hierarchy = 'sent_at'
+    raw_id_fields = ['sender', 'recipient', 'ad']
