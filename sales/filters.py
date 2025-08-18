ec457283b9d71dd4466e79f7bba633d592a06ca2
kumaraguhan7 <kumaraguhan@testpress.in>
2025-08-18 12:24:55 +0000

    Resolve it

--- sales/filters.py
+++ sales/filters.py
@@ -4,19 +4,20 @@ from django.utils.dateparse import parse_date
 from django.db.models import Q
 
 class AdFilter(django_filters.FilterSet):
-    q = django_filters.CharFilter(
+    keyword_to_search = django_filters.CharFilter(
         method="filter_search",
-        label="Search"
+        label="Search",
+        help_text="Search ads by keywords in the title or description."
     )
-    min_price = django_filters.NumberFilter(field_name="price", lookup_expr="gte")
-    max_price = django_filters.NumberFilter(field_name="price", lookup_expr="lte")
+    minimum_price = django_filters.NumberFilter(field_name="price", lookup_expr="gte")
+    maximum_price = django_filters.NumberFilter(field_name="price", lookup_expr="lte")
     category = django_filters.NumberFilter(field_name="category_id")
     location = django_filters.CharFilter(field_name="location", lookup_expr="icontains")
     event_date = django_filters.DateFilter(field_name="event_date")
 
     class Meta:
         model = Ad
-        fields = ["q", "category", "location", "min_price", "max_price", "event_date"]
+        fields = ["keyword_to_search", "category", "location", "minimum_price", "maximum_price", "event_date"]
 
     def filter_search(self, queryset, name, value):
         return queryset.filter(
