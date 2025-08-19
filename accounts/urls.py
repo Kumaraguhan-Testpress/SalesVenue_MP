a9efcda5742298ca041cda91203f8a983ba83afc
kumaraguhan7 <kumaraguhan@testpress.in>
2025-08-19 06:27:46 +0000

    Resolve Urls files

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,30 +9,22 @@ urlpatterns = [
     path('profile/', ProfileUpdateView.as_view(), name='profile'),
     path(
         'password_reset/',
-        auth_views.PasswordResetView.as_view(
-            template_name='registration/password_reset_form.html',
-        ),
+        auth_views.PasswordResetView.as_view(),
         name='password_reset'
     ),
     path(
         'password_reset/done/',
-        auth_views.PasswordResetDoneView.as_view(
-            template_name='registration/password_reset_done.html'
-        ),
+        auth_views.PasswordResetDoneView.as_view(),
         name='password_reset_done'
     ),
     path(
         'reset/<uidb64>/<token>/',
-        auth_views.PasswordResetConfirmView.as_view(
-            template_name='registration/password_reset_confirm.html',
-        ),
+        auth_views.PasswordResetConfirmView.as_view(),
         name='password_reset_confirm'
     ),
     path(
         'reset/done/',
-        auth_views.PasswordResetCompleteView.as_view(
-            template_name='registration/password_reset_complete.html'
-        ),
+        auth_views.PasswordResetCompleteView.as_view(),
         name='password_reset_complete'
     ),
 ]
